be3eaa5060a46ec7f2a702d453201056c4313440
victoriaaa111 <victoria.mutruc@gmail.com>
2025-02-14 12:57:37 +0200

    Add methods and attributes in Grammar class

--- src/main/java/Lab1/FiniteAutomaton.java
+++ src/main/java/Lab1/FiniteAutomaton.java
@@ -8,11 +8,13 @@ public class FiniteAutomaton
     private final Set<String> alphabet;
     private final String startState;
     private final Set<String> finalStates;
+    private final Set<String> states;
     private final Map<String, Map<String, String>> transitions;
 
 
-    public FiniteAutomaton(Set<String> alphabet, String startState, Set<String> finalStates, Map<String, Map<String, String>> transitions) {
+    public FiniteAutomaton(Set<String> alphabet, Set<String> states, String startState, Set<String> finalStates, Map<String, Map<String, String>> transitions) {
         this.alphabet = alphabet;
+        this.states = states;
         this.startState = startState;
         this.finalStates = finalStates;
         this.transitions = transitions;
--- src/main/java/Lab1/Grammar.java
+++ src/main/java/Lab1/Grammar.java
@@ -1,13 +1,71 @@
 package Lab1;
 
-import java.util.List;
+import java.util.*;
+
 
 public class Grammar
 {
-    private List<String> V_n;
-    private List<String> V_t;
-    private List<String> P;
-    private String S;
+    private final Set<String> V_n;
+    private final Set<String> V_t;
+    private final Map<String, List<String>> P;
+
+    public Grammar(Set<String> V_n, Set<String> V_t, Map<String, List<String>> P) {
+        this.V_n = V_n;
+        this.V_t = V_t;
+        this.P = P;
+    }
+
+    public String generateStrings(String startSymbol, int numStrings){
+        Set<String> generatedStrings = new HashSet<>();
+        while(generatedStrings.size() < numStrings){
+            StringBuilder sb = new StringBuilder();
+            generateString(startSymbol, sb);
+            generatedStrings.add(sb.toString());
+        }
+        return new ArrayList<>(generatedStrings).toString();
+    }
+
+    private void generateString(String symbol, StringBuilder sb) {
+        if (!this.P.containsKey(symbol)) {
+            sb.append(symbol);
+            return;
+        }
+        List<String> choices = this.P.get(symbol);
+        String choice = choices.get(new Random().nextInt(choices.size()));
+        for (char c : choice.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                generateString(String.valueOf(c), sb);
+            } else {
+                sb.append(c);
+            }
+        }
+    }
+
+    public FiniteAutomaton toFiniteAutomaton(){
+        Set<String> states = V_n;
+        Set<String> alphabet= V_t;
+        Map<String, Map<String, String>> transitions = new HashMap<>();
+
+        transitions.put("S", Collections.singletonMap("a", "B"));
+
+        transitions.put("B", new HashMap<String, String>(){{
+                put("b", "S");
+                put("a", "C");
+            }});
+
+        transitions.put("C", new HashMap<String, String>(){{
+            put("b", "D");
+        }});
+
+        transitions.put("D", new HashMap<String, String>(){{
+            put("b", "C");
+            put("c", "S");
+        }});
+
+        FiniteAutomaton finiteAutomaton = new FiniteAutomaton(alphabet, states, "S", new HashSet<>(Arrays.asList("B", "D")), transitions);
+
+        return finiteAutomaton;
+    }
 
 
 }
