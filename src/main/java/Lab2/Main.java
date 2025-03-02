7ab37dddc5d09190eaedb2ec68545a51e5692393
victoriaaa111 <victoria.mutruc@gmail.com>
2025-03-02 14:05:21 +0200

    Change classifying of grammars. Add functions to convert nfa to dfa, determine if fa is dfa and convert fa to regular grammar. Add main to test

--- src/main/java/Lab2/Main.java
+++ src/main/java/Lab2/Main.java
@@ -1,5 +1,6 @@
 package Lab2;
 
+
 import java.util.*;
 
 public class Main {
@@ -25,5 +26,45 @@ public class Main {
 
         String classification = grammar.classifyGrammar();
         System.out.println("Grammar Classification: " + classification);
+
+        FiniteAutomaton fa = new FiniteAutomaton();
+
+        Set<Character> alphabet = new HashSet<>();
+        alphabet.add('a');
+        alphabet.add('b');
+        alphabet.add('c');
+        fa.setAlphabet(alphabet);
+
+        fa.addState("q0");
+        fa.addState("q1");
+        fa.addState("q2");
+        fa.addState("q3");
+        fa.setStartState("q0");
+        fa.addFinalState("q3");
+        fa.addTransition("q0", 'a', "q0");
+        fa.addTransition("q0", 'a', "q1");
+        fa.addTransition("q1", 'b', "q2");
+        fa.addTransition("q2", 'c', "q3");
+        fa.addTransition("q3", 'c', "q3");
+        fa.addTransition("q2", 'a', "q2");
+
+        //convert fa to regular grammar
+        Grammar regularGrammar = fa.convertToRegularGrammar();
+        System.out.println("Regular Grammar:");
+        System.out.println(regularGrammar);
+
+
+        String result = regularGrammar.classifyGrammar();
+        System.out.println("Converted to Regular Grammar: " + ((result.equals("Type 3: Regular Grammar"))? "correct" : "incorrect"));
+
+        System.out.println("Finite Automaton is " + (fa.isDeterministic() ? "deterministic" : "non-deterministic"));
+
+        // convert NFA to DFA
+        if(!fa.isDeterministic()) {
+            FiniteAutomaton dfa = fa.convertToDFA();
+            System.out.println("Converted NFA to DFA:");
+            System.out.println(dfa);
+        }
+
     }
 }
