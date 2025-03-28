2cf298bacd39d3d3c21f9ba019c2ce05b589cb6a
victoriaaa111 <victoria.mutruc@gmail.com>
2025-03-28 11:01:50 +0200

    Add alternative selector class

--- src/main/java/Lab4/AlternativeSelector.java
+++ src/main/java/Lab4/AlternativeSelector.java
@@ -1,4 +1,14 @@
 package Lab4;
+import java.util.Random;
 
 public class AlternativeSelector {
+    private static final Random random = new Random();
+
+    public String select(String group) {
+        // Split alternatives
+        String[] alternatives = group.split("\\|");
+
+        // Randomly select an alternative
+        return alternatives[random.nextInt(alternatives.length)];
+    }
 }
